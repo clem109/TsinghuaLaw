9754f93e136bc8423a16830b61afec70584eb91f
clement venard <cvenard@me.com>
2016-12-20 10:29:18 +0800

    change to baseurl

--- _site/js/init.js
+++
@@ -1,7 +0,0 @@
-(function($){
-  $(function(){
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
-    $('.slider').slider({full_width: true, height: 700, indicators: false});
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
