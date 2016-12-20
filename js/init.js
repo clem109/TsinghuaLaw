ea01017556e88306f5b2db7ebae6f18486f0d7c6
clement venard <cvenard@me.com>
2016-12-20 11:21:59 +0800

    commiting

--- js/init.js
+++ js/init.js
@@ -3,5 +3,7 @@
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
     $('.slider').slider({full_width: true, height: 700, indicators: false});
+    $(".button-collapse").sideNav();
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
